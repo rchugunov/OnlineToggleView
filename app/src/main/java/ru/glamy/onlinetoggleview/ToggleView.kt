912519e07448b8c2f382a60271570351729694be
Roman Chugunov (Home macbook) <chugunov.r@gmail.com>
2018-07-08 23:57:10 +0300

    offset limits for scroll

--- app/src/main/java/ru/glamy/onlinetoggleview/ToggleView.kt
+++ app/src/main/java/ru/glamy/onlinetoggleview/ToggleView.kt
@@ -1,19 +1,24 @@
 package ru.glamy.onlinetoggleview
 
 import android.content.Context
-import android.graphics.Rect
-import android.support.v4.view.ViewCompat
-import android.support.v4.view.WindowInsetsCompat
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import android.widget.FrameLayout
 import ru.glamy.onlinetoggleview.databinding.ViewToggleBinding
 
 class ToggleView : FrameLayout {
 
     private lateinit var bnd: ViewToggleBinding
+    private lateinit var viewConfiguration: ViewConfiguration
+    private val leftLimit: Int = 0
+    private val rightLimit: Int = screenWidth()
+    private var mIsScrolling: Boolean = false
+    private var moveStartedPosition: Float = 0f
+    private var scrollStartedPosition = 0f
+    private var scrollCurrentPosition = 0f
 
     constructor(context: Context) : super(context) {
 
@@ -33,77 +38,79 @@ class ToggleView : FrameLayout {
     private fun init() {
         bnd = ViewToggleBinding.inflate(layoutInflater(), this, false)
         bnd.rightPart.layoutParams.width = screenWidth()
-        clipChildren = false
+        viewConfiguration = ViewConfiguration.get(context)
         addView(bnd.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(this,
-                object : android.support.v4.view.OnApplyWindowInsetsListener {
-                    private val mTempRect = Rect()
-
-                    override fun onApplyWindowInsets(v: View,
-                                                     originalInsets: WindowInsetsCompat): WindowInsetsCompat {
-                        // First let the ViewPager itself try and consume them...
-                        val applied = ViewCompat.onApplyWindowInsets(v, originalInsets)
-                        if (applied.isConsumed) {
-                            // If the ViewPager consumed all insets, return now
-                            return applied
-                        }
-
-                        // Now we'll manually dispatch the insets to our children. Since ViewPager
-                        // children are always full-height, we do not want to use the standard
-                        // ViewGroup dispatchApplyWindowInsets since if child 0 consumes them,
-                        // the rest of the children will not receive any insets. To workaround this
-                        // we manually dispatch the applied insets, not allowing children to
-                        // consume them from each other. We do however keep track of any insets
-                        // which are consumed, returning the union of our children's consumption
-                        val res = mTempRect
-                        res.left = applied.systemWindowInsetLeft
-                        res.top = applied.systemWindowInsetTop
-                        res.right = applied.systemWindowInsetRight
-                        res.bottom = applied.systemWindowInsetBottom
-
-                        var i = 0
-                        val count = childCount
-                        while (i < count) {
-                            val childInsets = ViewCompat
-                                    .dispatchApplyWindowInsets(getChildAt(i), applied)
-                            // Now keep track of any consumed by tracking each dimension's min
-                            // value
-                            res.left = Math.min(childInsets.systemWindowInsetLeft,
-                                    res.left)
-                            res.top = Math.min(childInsets.systemWindowInsetTop,
-                                    res.top)
-                            res.right = Math.min(childInsets.systemWindowInsetRight,
-                                    res.right)
-                            res.bottom = Math.min(childInsets.systemWindowInsetBottom,
-                                    res.bottom)
-                            i++
-                        }
-
-                        // Now return a new WindowInsets, using the consumed window insets
-                        return applied.replaceSystemWindowInsets(
-                                res.left, res.top, res.right, res.bottom)
-                    }
-                })
+        initialOffsets()
     }
 
-    private fun screenWidth(): Int =
-            resources.displayMetrics.widthPixels
+    private fun initialOffsets() {
+        moveView(bnd.leftPart, (screenWidth() - bnd.leftPart.layoutParams.width).toFloat())
+        moveView(bnd.rightPart, screenWidth().toFloat())
+    }
 
-    private fun layoutInflater(): LayoutInflater =
-            LayoutInflater.from(context)
+    private fun calculateDistanceX(ev: MotionEvent): Int = ev.x.toInt() - moveStartedPosition.toInt()
 
-    private var startedPosition: Float = 0f
+    private fun screenWidth(): Int = resources.displayMetrics.widthPixels
+
+    private fun layoutInflater(): LayoutInflater = LayoutInflater.from(context)
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+
+        val action = ev?.action
+
+        // Always handle the case of the touch gesture being complete.
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            // Release the scroll.
+            mIsScrolling = false
+            return false // Do not intercept touch event, let the child handle it
+        }
+
+        when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                moveStartedPosition = ev.x
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (mIsScrolling) {
+                    // We're currently scrolling, so yes, intercept the
+                    // touch event!
+                    return true
+                }
+
+                // If the user has dragged her finger horizontally more than
+                // the touch slop, start the scroll
+
+                // left as an exercise for the reader
+                val xDiff = calculateDistanceX(ev)
+
+                // Touch slop should be calculated using ViewConfiguration
+                // constants.
+                if (xDiff > viewConfiguration.scaledTouchSlop) {
+                    // Start scrolling!
+                    mIsScrolling = true
+                    return true
+                }
+            }
+        }
+
+        // In general, we don't want to intercept touch events. They should be
+        // handled by the child view.
+        return false
+    }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
-                startedPosition = event.x
+                scrollStartedPosition = event.x
+                scrollCurrentPosition = event.x
                 return true
             }
             MotionEvent.ACTION_MOVE -> {
-                moveView(event.x)
-                startedPosition = event.x
+                val delta = event.x - scrollCurrentPosition
+                val validDelta = calculateValidDelta(bnd.rightPart.left.toFloat(), delta)
+                moveView(bnd.leftPart, validDelta)
+                moveView(bnd.rightPart, validDelta)
+                scrollCurrentPosition = event.x
                 return true
             }
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
@@ -116,7 +123,26 @@ class ToggleView : FrameLayout {
         }
     }
 
-    private fun moveView(x: Float) {
-        bnd.root.offsetLeftAndRight(x.toInt() - startedPosition.toInt())
+    private fun calculateValidDelta(viewLeft: Float, delta: Float): Float {
+        var resultDelta = delta
+        if (viewLeft + delta > rightLimit) {
+            resultDelta = rightLimit - viewLeft
+        }
+
+        if (viewLeft + delta < leftLimit) {
+            resultDelta = leftLimit - viewLeft
+        }
+
+        return resultDelta
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        initialOffsets()
+    }
+
+    private fun moveView(view: View, delta: Float) {
+        view.offsetLeftAndRight(delta.toInt())
     }
 }
