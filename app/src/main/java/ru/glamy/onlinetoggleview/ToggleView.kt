3f9b67b7ad029dda0fd4e66d3d2332ed76152168
Roman Chugunov (Home macbook) <chugunov.r@gmail.com>
2018-07-09 00:35:45 +0300

    spring animation added

--- app/src/main/java/ru/glamy/onlinetoggleview/ToggleView.kt
+++ app/src/main/java/ru/glamy/onlinetoggleview/ToggleView.kt
@@ -1,11 +1,12 @@
 package ru.glamy.onlinetoggleview
 
 import android.content.Context
+import android.support.animation.DynamicAnimation.MIN_VISIBLE_CHANGE_PIXELS
+import android.support.animation.FloatValueHolder
+import android.support.animation.SpringAnimation
+import android.support.animation.SpringForce
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewConfiguration
+import android.view.*
 import android.widget.FrameLayout
 import ru.glamy.onlinetoggleview.databinding.ViewToggleBinding
 
@@ -19,6 +20,11 @@ class ToggleView : FrameLayout {
     private var moveStartedPosition: Float = 0f
     private var scrollStartedPosition = 0f
     private var scrollCurrentPosition = 0f
+    var friction = 1f
+    private var circleX = 0f
+    private var xVelocity = 0f
+    private var velocityTracker: VelocityTracker? = null
+    private var xFling: SpringAnimation? = null
 
     constructor(context: Context) : super(context) {
 
@@ -101,11 +107,28 @@ class ToggleView : FrameLayout {
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
+                stopFlingAnimations()
+                if (velocityTracker == null) {
+                    velocityTracker = VelocityTracker.obtain()
+                } else {
+                    velocityTracker?.clear()
+                }
+                velocityTracker?.addMovement(event)
+
                 scrollStartedPosition = event.x
                 scrollCurrentPosition = event.x
                 return true
             }
             MotionEvent.ACTION_MOVE -> {
+
+                if (friction < MAX_FRICTION) {
+                    velocityTracker?.let {
+                        it.addMovement(event)
+                        it.computeCurrentVelocity(500, 10000f)
+                        xVelocity = it.getXVelocity(event.getPointerId(0))
+                    }
+                }
+
                 val delta = event.x - scrollCurrentPosition
                 val validDelta = calculateValidDelta(bnd.rightPart.left.toFloat(), delta)
                 moveView(bnd.leftPart, validDelta)
@@ -113,15 +136,54 @@ class ToggleView : FrameLayout {
                 scrollCurrentPosition = event.x
                 return true
             }
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                return true
+            MotionEvent.ACTION_UP -> {
+                velocityTracker?.recycle()
+                velocityTracker = null
+                if (friction < MAX_FRICTION) {
+                    startXFlingAnimation(xVelocity < 0)
+                }
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                velocityTracker?.recycle()
+                velocityTracker = null
 
             }
-            else -> {
-                return super.onTouchEvent(event)
-            }
         }
+        return true
     }
+
+    private fun startXFlingAnimation(springToLeft: Boolean) {
+        val springFinalPosition = calculateSpringFinalPosition(springToLeft)
+        xFling = SpringAnimation(FloatValueHolder(bnd.rightPart.left.toFloat()))
+//                .setStartVelocity(xVelocity)
+//                .setMaxValue(rightLimit.toFloat())
+//                .setMinValue(leftLimit.toFloat())
+                .setMinimumVisibleChange(MIN_VISIBLE_CHANGE_PIXELS)
+                .setSpring(SpringForce(springFinalPosition)
+                        .apply {
+                            stiffness = SpringForce.STIFFNESS_LOW
+                            dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
+                        })
+//                .setFriction(friction)
+                .apply {
+                    addUpdateListener({ _, newX, _ ->
+                        moveView(bnd.leftPart, newX - bnd.rightPart.left)
+                        moveView(bnd.rightPart, newX - bnd.rightPart.left)
+                    })
+                    addEndListener({ _, canceled, _, velocity ->
+                        if (!canceled && Math.abs(velocity) > 0) {
+                            xVelocity = -velocity
+                            startXFlingAnimation(xVelocity < 0)
+                        }
+                    })
+                    start()
+                }
+    }
+
+    private fun calculateSpringFinalPosition(springToLeft: Boolean): Float {
+        return if (springToLeft) leftLimit.toFloat() else rightLimit.toFloat()
+    }
+
 
     private fun calculateValidDelta(viewLeft: Float, delta: Float): Float {
         var resultDelta = delta
@@ -136,6 +198,11 @@ class ToggleView : FrameLayout {
         return resultDelta
     }
 
+    private fun stopFlingAnimations() {
+        xFling?.cancel()
+        xFling = null
+    }
+
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
 
@@ -144,5 +211,9 @@ class ToggleView : FrameLayout {
 
     private fun moveView(view: View, delta: Float) {
         view.offsetLeftAndRight(delta.toInt())
+    }
+
+    companion object {
+        val MAX_FRICTION = Float.MAX_VALUE
     }
 }
